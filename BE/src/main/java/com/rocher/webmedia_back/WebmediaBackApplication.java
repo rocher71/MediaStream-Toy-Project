d1e5fbbd65e579c4a8e6b1f3c3a7e59581233069
yejin <qja086351@naver.com>
2025-07-22 16:49:05 +0900

    be settings

---
+++ BE/src/main/java/com/rocher/webmedia_back/WebmediaBackApplication.java
@@ -0,0 +1,13 @@
+package com.rocher.webmedia_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmediaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmediaBackApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/rocher/webmedia_back/WebmediaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocher.webmedia_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmediaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
